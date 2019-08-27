9e3d2eef974c8cafe546d99451cf26e9c3376036
M Yakub Mizan <6322.ym@gmail.com>
2019-08-28 03:12:37 +0600

    Initial release

---
+++ README.md
@@ -0,0 +1,6 @@
+# WP SHOW CATEGORY ID
+A simple plugin that will apply a CSS stylesheet globally across all sites of a WordPress multi-site installation. 
+
+There are many other CSS plugins which do the job better, but the goal here is simpsicity. As developers, we often need to apply small piece of CSS and a fancy plugin is an overkill for it. I wrote this plugin so that I can put small piece of CSS code that are unrelated to any plugin or themes, without having to install install a complex, bloated plugin. 
+
+Hope you will find it useful. 
---
+++ wp-show-category-id.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Plugin Name: WP Show Category ID
+ * Plugin URI: https://yakub.xyz
+ * Description: A simple plugin to show category IDs for post categories and WooCommerce product caegories
+ * Author: M Yakub Mizan
+ * Version: 1.0.0
+ * Author URI: https://yakub.xyz
+ **/
+
+
+define( 'WP_SHOW_CAT_IDS_NAME', 'WP Show Category ID' );
+define( 'WP_SHOW_CAT_IDS_VERSION', '1.0.0' );
+
+class WP_SHOW_CAT_IDS_MAIN {
+
+	/**
+	 * All our action hooks and filters here
+	 * so that they are regiestered when the
+	 * plugin is initiated.
+	 */
+	public function __construct() {
+		add_filter( 'term_name', array( $this, 'show_category_id' ), 10, 1 );
+	}
+
+	public function show_category_id( $term_name ) {
+
+		if ( $this->is_showing_category_list() ) { 
+			$cat_id = $this->__get_term_id( $term_name );
+
+			if ( $cat_id != 0 ) {
+				return  $term_name . ' (ID:'. $cat_id . ') ';
+			} else {
+				return $term_name;
+			}
+		}
+
+		return $term_name;
+	}
+
+	public function is_showing_category_list() {
+		$screen = get_current_screen();
+
+		if ( $screen->id == 'edit-product_cat' || $screen->id == 'edit-category' ) {
+			return true;
+		}
+
+		return;
+	}
+
+	public function __get_term_id( $term_name ) {
+		$screen = get_current_screen();
+
+		if ( $screen->id == 'edit-category' ) {
+			return get_cat_ID( $term_name );;
+		}
+
+		if ( $screen->id == 'edit-product_cat' ) {
+			$_term =  get_term_by('name', $term_name , 'product_cat');
+
+			if ( $_term ) {
+				return $_term->term_id;
+			}
+		}
+	}
+}
+
+
+new WP_SHOW_CAT_IDS_MAIN();
